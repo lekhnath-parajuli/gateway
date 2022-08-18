25c2a0fabef27f00a9d216b736a8c6c02a8ba3bd
Lekhnath Parajuli <lekhnathparajuli4@gmail.com>
2022-08-18 21:58:40 +0545

    Add message consumer

---
+++ backend/Consumer.py
@@ -0,0 +1,18 @@
+from aio_pika import connect
+import asyncio
+from typing import Callable
+
+from config import config
+
+
+class Consumer:
+    def __init__(self, service: str) -> None:
+        self.service = service
+
+    async def listen(self, callback: Callable):
+        connection = await connect(config.RABBITMQ_SERVER)
+        async with connection:
+            channel = await connection.channel()
+            queue = await channel.declare_queue(self.service)
+            await queue.consume(callback=callback)
+            # await asyncio.Future()
